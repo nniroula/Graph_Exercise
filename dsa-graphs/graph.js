5f20e16627516ea023d2d5fd1d3c6d93dbbcbec5
nabin <nkbhaie2017@gmail.com>
2022-01-26 15:06:14 -0700

    add two functions

--- dsa-graphs/graph.js
+++ dsa-graphs/graph.js
@@ -11,16 +11,27 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+      this.nodes.add(vertex);
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+      for(let point of vertexArray){
+          this.addVertex(point);
+      }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+      v1.adjacent.add(v2);
+      v2.adjacent.add(v1);
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+      
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
   removeVertex(vertex) {}
